43035940df585d88035efda0be6a8d4fcf17b0c6
swastik <swastikasija072@gmail.com>
2024-04-03 12:28:17 +0530

    NewInvitations  section added

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,20 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import NewInvitation from './components/NewInvitation'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Navbar/>
+    <BrowserRouter>
+    <Routes>
+    <Route path='/' element={<Navbar/>}/>
+    <Route path='/newinvitation' element={<NewInvitation/>}/>
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
